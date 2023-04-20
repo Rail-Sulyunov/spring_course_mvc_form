b9780c46f2c7b0fc36ddd53f04ae1c660fba56cc
Rail Sulyunov <Sulyunov.r@yandex.ru>
2023-04-20 11:15:39 +0400

    Завершение Валидации MVC

--- src/main/java/com/railsulyunov/spring/mvc/MyController.java
+++ src/main/java/com/railsulyunov/spring/mvc/MyController.java
@@ -1,12 +1,15 @@
 package com.railsulyunov.spring.mvc;
 
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
-import javax.servlet.http.HttpServletRequest;
+
+
+
 
 @Controller
 //@RequestMapping("/employee")
@@ -29,18 +32,13 @@ public class MyController {
     }
 
     @RequestMapping("/showDetails")
-    public String showEmpDetails(@ModelAttribute("employee") Employee emp) {
+    public String showEmpDetails(@Valid @ModelAttribute("employee") Employee emp
+            , BindingResult bindingResult) {
 
-        String name = emp.getName();
-        emp.setName("Mr. " + name);
-
-        String surname = emp.getSurname();
-        emp.setSurname(surname + "!");
-
-        int salary = emp.getSalary();
-        emp.setSalary(salary * 10);
-
-        return "show-amp-details-view";
+        if (bindingResult.hasErrors()) {
+            return "ask-emp-details-view";
+        } else {
+            return "show-amp-details-view";
+        }
     }
-
 }
--- src/main/webapp/WEB-INF/view/show-amp-details-view.jsp
+++ src/main/webapp/WEB-INF/view/show-amp-details-view.jsp
@@ -1,3 +1,5 @@
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ taglib prefix="form" uri="http://www.springframework.org/tags/form" %>
 <!DOCTYPE html>
 <html>
 
@@ -12,10 +14,22 @@
 <%--Your name: ${param.employeeName}--%>
 
 Your name: ${employee.name}
-<br>
+<br><br>
 Your surname: ${employee.surname}
-<br>
+<br><br>
 Your salary: ${employee.salary}
+<br><br>
+Your department: ${employee.department}
+<br><br>
+Your car: ${employee.carBrand}
+<br><br>
+Language(s):
+<ul>
+    <c:forEach var ="lang" items="${employee.languages}">
+        <li>   ${lang}   </li>
+    </c:forEach>
+
+</ul>
 
 </body>
 
